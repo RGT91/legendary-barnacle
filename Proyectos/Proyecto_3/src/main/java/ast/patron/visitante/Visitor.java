13dc9a9fb83a5b42729f7238a2b5f7e36abf645a
RGT91 <richardgt91@gmail.com>
2018-05-11 14:58:42 -0500

    Agregué operadores básicos.

--- Proyectos/Proyecto_3/src/main/java/ast/patron/visitante/Visitor.java
+++ Proyectos/Proyecto_3/src/main/java/ast/patron/visitante/Visitor.java
@@ -12,6 +12,17 @@ public interface Visitor
     public void visit(IfNodo n);
     public void visit(PrintNodo n);
     public void visit(IfElseNodo n);
+    public void visit(MultNodo n);
+    public void visit(DivNodo n);
+    public void visit(PotNodo n);
+    public void visit(LTNodo n);
+    public void visit(GTNodo n);
+    public void visit(LENodo n);
+    public void visit(GENodo n);
+    public void visit(EqNodo n);
+    public void visit(NEqNodo n);
+    public void visit(ModNodo n);
+    public void visit(DivENodo n);
     public void visit(Hoja n);
     public void visit(IdentifierHoja n);
     public void visit(IntHoja n);
